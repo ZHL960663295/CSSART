6bf848e15f7676272ac45290ebd44166638e86d3
ZHL960663295 <960663295@qq.com>
2021-03-09 14:02:06 +0800

    Add files via upload

---
+++ cssart/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <shengbeilayout />
+  </div>
+</template>
+
+<script>
+import shengbeilayout from './components/layout/shengbeilayout'
+
+export default {
+  name: 'App',
+  components: {
+    shengbeilayout
+  }
+}
+</script>
+
+<style>
+*{
+    margin: 0;
+    padding: 0;
+}
+</style>
---
+++ cssart/src/components/layout/shengbeilayout.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="grail-layout">
+      <div class="left"></div>
+      <div class="right"></div>
+      <div class="center">dfsjflsjfl裂开时间的分类开始减肥楼上的几点上课了废旧塑料大嫁风尚副经理圣诞节福利时代峻峰世纪东方抗裂砂浆</div>
+  </div>
+</template>
+
+<script>
+export default {
+    name:'shengbeilayout'
+
+}
+</script>
+
+<style lang="less" scoped>
+
+.grail-layout{
+    padding: 0 100px;
+    width: 400px;
+    height: 400px;
+}
+.left{
+    float: left;
+    width: 100px;
+    height: 100%;
+    margin-left: -100px;
+    background-color: #f66;
+}
+.right{
+    float:right;
+    width: 100px;
+    height: 100%;
+    margin-right: -100px;
+    background-color: #66f;
+}
+.center{
+    height: 100%;
+    background-color: #3c9;
+}
+
+</style>
