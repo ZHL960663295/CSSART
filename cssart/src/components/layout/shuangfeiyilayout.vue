c8e6bc778620eef6a0a1311be7900b40b769e4a3
zero <zhaohengliang@saimo.ai>
2021-03-09 14:51:06 +0800

    feat:新增.gitignore文件

--- cssart/src/components/layout/shuangfeiyilayout.vue
+++ cssart/src/components/layout/shuangfeiyilayout.vue
@@ -1,26 +1,22 @@
-<!--  -->
+<!-- 双飞翼布局 -->
 <template>
-<div></div>
+  <div class="grail-layout">
+      <div class="left"></div>
+      <div class="left"></div>
+  </div>
 </template>
 
 <script>
 export default {
-data() {
-return {
-
-}
-},
-//生命周期 - 创建完成（访问当前this实例）
-created() {
-
-},
-//生命周期 - 挂载完成（访问DOM元素）
-mounted() {
-
-}
-}
+  data() {
+    return {};
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted() {},
+};
 </script>
 <style scoped>
 /* @import url(); 引入css类 */
-
 </style>
